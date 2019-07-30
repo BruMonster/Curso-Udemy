77d8b145f4af117db6ecfb8cd1d003d437687da1
Bruno Gabriel <52282554+BruMonster@users.noreply.github.com>
2019-07-30 05:15:15 -0300

    Add files via upload

---
+++ fundamentos/math.js
@@ -0,0 +1,7 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+//Entre parenteses é referente ao raio elevado ao quadrado
+
+console.log(area)
+console.log(typeof Math)
+//Math é um objeto e não uma constante
